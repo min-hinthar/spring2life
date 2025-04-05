88e40c4abbed7dc35ef5854c6f6ab8043e89f9b9
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2025-04-04 23:19:53 -0700

    homepage updated with logo, side image, copyright, patient form tsx created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,44 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click Me</Button>
+    <div className="flex h-screen max-h-screen">
+
+      {/* TODO: OTP Verification | PasskeyModal */}
+
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]"> 
+          <Image 
+            src="/spring2life.png"
+            width={10000}
+            height={10000}
+            alt="logo"
+            className="mb-12 h-20 w-fit rounded-3xl shadow-green-900 shadow-md"
+          />
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2025 Spring2Life
+            </p>
+
+            <Link href="/admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image 
+        src="/assets/images/karenIDP.jpg"
+        width={1000}
+        height={1000}
+        alt="patientForm"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
